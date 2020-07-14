92ef7967085a366ac99487020b274301278eaf16
Debora Polesel <deborahpolesel@hotmail.com>
2020-07-14 15:21:31 -0300

    Correção exercicio 2 e resolução exercicio 3

--- 4_2/exercicio02.js
+++ 4_2/exercicio02.js
@@ -3,7 +3,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var soma = 0;
 
-for (i=0; i<numbers.length; i++){
+for (let i=0; i<numbers.length; i++){
     soma += numbers[i];
 }
-console.log (soma)
+console.log (soma);
